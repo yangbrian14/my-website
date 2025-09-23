20771861b436600f1b3937a96c5f5e036c4792a3
yangbrian14 <38969605+yangbrian14@users.noreply.github.com>
2025-09-23 11:28:43 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: 'src\pages\Projects.js',
+                to: 'src\pages\Projects',
               },
             ],
           },
